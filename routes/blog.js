3d03a84bd6ca0d452f31985dc8b3132171f470b4
stanzhai <jasondan325@163.com>
2014-05-31 21:52:42 +0800

    change dao to thunk model, simplify blog route functions

--- routes/blog.js
+++ routes/blog.js
@@ -22,43 +22,22 @@ exports.index = function *(page_index){
 
   // post count of per page
   var page_size = config.per_page;
+  var totalCount = yield PostDao.count(search);
 
-  var results = yield function (fn) {
-    async.parallel({
-      count: function (callback) {
-        // get all public post count
-        PostDao.count(search, function (err, count) {
-          if (err) {
-            callback(err, null);
-          } else {
-            callback(null, count);
-          }
-        });
-      },
-      posts: function (callback) {
-        // limit fields return
-        var fields = {title: 1, url: 1, create_at: 1, public: 1, views: 1, abstract: 1};
-        // search options
-        var opt = {sort: {create_at: -1}, skip: (page_index - 1) * page_size, limit: page_size};
-        PostDao.find(search, fields, opt, function (err, data) {
-          if (err) {
-            callback(err, null);
-          } else {
-            callback(null, data);
-          }
-        });
-      }
-    }, fn);
-  };
+  // limit fields return
+  var fields = {title: 1, url: 1, create_at: 1, public: 1, views: 1, abstract: 1};
+  // search options
+  var opt = {sort: {create_at: -1}, skip: (page_index - 1) * page_size, limit: page_size};
+  var posts = yield PostDao.find(search, fields, opt);
 
   // total post list page count
-  var pages = results.count / page_size + results.count % page_size;
+  var pages = totalCount / page_size + totalCount % page_size;
 
   var indexTitle = page_index == 1 ? '首页' : '';
   var preTitle = keywords != '' ? '搜索:' + keywords : indexTitle;
   var title = page_index == 1 ? preTitle : preTitle + '第 ' + page_index + ' 页';
 
-  var render_data = { posts: results.posts, title: title, current_page: page_index, total_page: pages};
+  var render_data = { posts: posts, title: title, current_page: page_index, total_page: pages};
   this.body = yield render('list', render_data); 
 };
 
@@ -66,43 +45,18 @@ exports.index = function *(page_index){
 exports.posts = function *(next) {
   var url = this.path.replace('/', '');
   url = decodeURI(url);
-  var post = yield function (fn) {
-    PostDao.findOne({url: url, public: true}, fn);
-  };
+  var post = yield PostDao.findOne({url: url, public: true});
   if (post) {
-    var results = yield function (fn) {
-      var fields = {url: 1, title: 1};
-      // search options
-      var opt = {sort: {create_at: -1}, limit: 1};
-      // get prev and next post
-      async.parallel({
-        prev: function (callback) {
-          var search = {public: true, create_at: {$gt: post.create_at}};
-          PostDao.find(search, fields, opt, function (err, data) {
-            if (err) {
-              callback(err, null);
-            } else {
-              callback(null, data[0]);
-            }
-          });
-        },
-        next: function (callback) {
-          var search = {public: true, create_at: {$lt: post.create_at}};
-          PostDao.find(search, fields, opt, function (err, data) {
-            if (err) {
-              callback(err, null);
-            } else {
-              callback(null, data[0]);
-            }
-          });
-        }
-      },fn);
-    };
+    var fields = {url: 1, title: 1};
+    // search options
+    var opt = {sort: {create_at: -1}, limit: 1};
+    var search = {public: true, create_at: {$gt: post.create_at}};
+    post.prev = yield PostDao.find(search, fields, opt);
+    search = {public: true, create_at: {$lt: post.create_at}};
+    post.next = yield PostDao.find(search, fields, opt);
     // add view count
     PostDao.update({url: url}, {views: post.views + 1});
     // set the prev and next post
-    post.prev = results.prev;
-    post.next = results.next;
     this.body = yield render('post', post);
   }
   yield next;
