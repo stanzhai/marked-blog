e9c25fceecc9c52a3e0e9ac1fcb6955483706336
Stan Zhai <mail@stanzhai.site>
2018-02-12 12:39:14 +0800

    update post model

--- app/model/post.lua
+++ app/model/post.lua
@@ -4,9 +4,23 @@ local db = DB:new()
 local post_model = {}
 
 function post_model:create(title, slug, markdown)
-    local res, err = db:query("insert into post(title, slug, markdown) values(?,?,?,?)", 
+    return db:query("insert into post(title, slug, markdown) values(?,?,?,?)", 
     	{title, slug, markdown})
-    return res, err
+end
+
+function post_model:update(post_id, title, markdown)
+    return db:query("update post set title=? markdown=? where id=?",
+        {title, markdown, tonumber(post_id)})
+end
+
+function post_model:delete(post_id)
+    local res, err = db:query("delete from post where id=?",
+        {tonumber(post_id)})
+    if res and not err then
+        return true
+    else
+        return false
+    end
 end
 
 return post_model
